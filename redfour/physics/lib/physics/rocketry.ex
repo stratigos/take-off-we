3a32ab285c81d0686f4969d9bd0fc9d106b50597
Todd Morningstar <techarchist@gmail.com>
2018-01-16 23:44:23 -0500

    Migrate Converter to its own file
     * Ch 7

--- redfour/physics/lib/physics/rocketry.ex
+++ redfour/physics/lib/physics/rocketry.ex
@@ -1,70 +1,3 @@
-defmodule ConverterTwo do
-
-  def to_nearest_tenth(val) do
-    round_to(val, 1)
-  end
-
-  def to_km(val) do
-    val / 1000
-  end
-
-  def to_meters(val) do
-    val * 1000
-  end
-
-  def to_light_seconds(arg), do: to_light_seconds(arg, precision: 5)
-  def to_light_seconds({unit, val}, precision: precision) do
-    light_seconds = case unit do
-      :miles -> from_miles(val)
-      :meters -> from_meters(val)
-      :feet -> from_feet(val)
-      :inches -> from_inches(val)
-    end
-    light_seconds |> round_to(precision) # example 2
-  end
-
-  defp from_miles(val), do: val * 5.36819e-6
-  defp from_meters(val), do: val * 3.335638620368e-9
-  defp from_feet(val), do: val * 1.016702651488166404e-9
-  defp from_inches(val), do: val * 8.472522095734715723e-11
-  defp round_to(val, precision), do: Float.round(val, precision)
-
-end
-
-defmodule Converter do
-  def to_nearest_tenth(val) do
-    round_to(val, 1)
-  end
-
-  def to_km(val) do
-    val / 1000
-  end
-
-  def to_meters(val) do
-    val * 1000
-  end
-
-  def to_light_seconds({:miles, miles} = val, precision: precision) do
-    (miles * 5.36819e-6) |> round_to(precision)
-  end
-
-  def to_light_seconds({:meters, meters} = val, precision: precision)  do
-    (meters * 3.335638620368e-9) |> round_to(precision)
-  end
-
-  def to_light_seconds({:feet, feet} = val, precision: precision)  do
-    (feet * 1.016702651488166404e-9) |> round_to(precision)
-  end
-
-  def to_light_seconds({:inches, inches} = val, precision: precision)  do
-    (inches * 8.472522095734715723e-11) |> round_to(precision)
-  end
-
-  defp round_to(val, precision) when is_float(val) do
-    Float.round(val, precision)
-  end
-end
-
 # Do all of the things cohesive to the whooshing and screaching of rockets.
 defmodule Physics.Rocketry do
 
@@ -73,6 +6,14 @@ defmodule Physics.Rocketry do
   # - The mass of Mars is 6.39e23kg and its radius is 3.4e6m.
   # - The mass of the Moon is 7.35e22kg and its radius is 1.73e6m.
   #################################################################
+
+  import Calcs
+  import Converter
+
+  # Use Module attributes to remove data from function signatures
+  @moon  %{ mass: 7.35e22, radius: 1.738e6 }
+  @mars  %{ mass: 6.39e23, radius: 3.4e6 }
+  @earth %{ mass: 5.972e24, radius: 6.372e6 }
 
   def escape_velocity(:earth) do
     %{ mass: 5.972e24, radius: 6.371e6 }
