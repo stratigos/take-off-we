c869697e8e320d80176886256ec5889679a24208
Todd Morningstar <techarchist@gmail.com>
2018-01-17 11:24:41 -0500

    Add example of debugging via IO inspection

--- redfour/physics/test/argument_error_test.exs
+++ redfour/physics/test/argument_error_test.exs
@@ -5,8 +5,11 @@ defmodule ArgumentErrorTest do
     "Hello #{name}, I see that you're #{opts[:age]} years old"
   end
 
-  test "a non-compiling Erlang error" do
-    val = say_hello "Steve", age: 12
+  # Previously this was broken by not referencing the String module.
+  test "a noncompiling Erlang error" do
+    val = say_hello("Steve", age: 12)
+      |> IO.inspect # "Knuckle-Drag Debugging" eh? Nothing wrong with tried and true classics ;-)
+
     assert String.length(val) > 10
   end
 end
