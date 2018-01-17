27735e4675963e8cdef2a30da1db7d87f76aeccd
Todd Morningstar <techarchist@gmail.com>
2018-01-17 11:28:20 -0500

    Add example of IEx pry for debugging

--- redfour/physics/test/argument_error_test.exs
+++ redfour/physics/test/argument_error_test.exs
@@ -1,5 +1,6 @@
 defmodule ArgumentErrorTest do
   use ExUnit.Case
+  # require IEx # IEx "pry" debugger feature
 
   def say_hello(name, opts) do
     "Hello #{name}, I see that you're #{opts[:age]} years old"
@@ -7,8 +8,11 @@ defmodule ArgumentErrorTest do
 
   # Previously this was broken by not referencing the String module.
   test "a noncompiling Erlang error" do
-    val = say_hello("Steve", age: 12)
-      |> IO.inspect # "Knuckle-Drag Debugging" eh? Nothing wrong with tried and true classics ;-)
+    # val = say_hello("Steve", age: 12)
+    #   |> IO.inspect # "Knuckle-Drag Debugging" eh? Nothing wrong with tried and true classics ;-)
+    val = say_hello "Steve", age: 12
+
+    # IEx.pry # Open debug console here, when called inside IEx
 
     assert String.length(val) > 10
   end
