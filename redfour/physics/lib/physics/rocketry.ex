6c07452a1b8f114d932c5df80ee11ab710593822
Todd Morningstar <techarchist@gmail.com>
2018-01-16 23:52:05 -0500

    Leverage pattern matching to simplify calculating escape velocity in Rocketry
     * Halfway through Ch 7

--- redfour/physics/lib/physics/rocketry.ex
+++ redfour/physics/lib/physics/rocketry.ex
@@ -7,23 +7,23 @@ defmodule Physics.Rocketry do
   import Planets
 
   def escape_velocity(:earth) do
-    %{ mass: 5.972e24, radius: 6.371e6 }
-     |> escape_velocity
+    earth
+      |> escape_velocity
   end
 
-  # Calculate the necessary escape velocity for planetary gravity.
+  def escape_velocity(:mars) do
+    mars
+      |> escape_velocity
+  end
+
+  def escape_velocity(:moon) do
+    moon
+      |> escape_velocity
+  end
   def escape_velocity(planet) when is_map(planet) do
     planet
       |> calculate_escape
-      |> Converter.to_km
-      |> Converter.to_nearest_tenth
+      |> to_km
+      |> to_nearest_tenth
   end
-
-  defp calculate_escape(%{ mass: mass, radius: radius }) do
-    newtons_constant = 6.67e-11
-
-    2 * newtons_constant * mass / radius
-     |> :math.sqrt
-  end
-
 end
