2881776560d6d8fb8d2b90017fa9425ff0b84287
Todd Morningstar <techarchist@gmail.com>
2018-01-23 20:46:58 -0500

    Implement Solar module
     * Create functions for calculating "power" of a solar flare
     * Create function for determining safe limit of flare power

---
+++ redfour/physics/lib/solar.ex
@@ -0,0 +1,21 @@
+defmodule Solar do
+
+  def power(%{classification: :C, scale: s}), do: s * 1
+  def power(%{classification: :M, scale: s}), do: s * 10
+  def power(%{classification: :X, scale: s}), do: s * 1000
+
+  # Long version for function definition:
+  # def no_eva(flares) do
+  #   Enum.filter flares, fn(flare) ->
+  #     power(flare) > 1000
+  #   end
+  # end
+  #
+  # The following shorthand can be extrapolated to the above
+  #  commented block:
+  # Note:
+  #  `&()` denotes an anonymous function
+  #  `&1` denotes the first argument passed into current func scope
+  def no_eva(flares), do: Enum.filter flares, &(power(&1) > 1000)
+
+end
