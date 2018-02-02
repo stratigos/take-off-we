0133d013e8580f1ac0f01da144e46fd578a495cc
Todd Morningstar <techarchist@gmail.com>
2018-02-01 19:17:33 -0500

    Add recursive reducer for total flare power to Solar module.
     * Ch 11 intro
     * See notes in Solar.ex
     * Use pattern matching in function signature to perform different operations on different flare types
      * and avoid any if/unless conditionals

--- redfour/physics/lib/solar.ex
+++ redfour/physics/lib/solar.ex
@@ -23,4 +23,48 @@ defmodule Solar do
       |> Enum.max
   end
 
+  # Recursion in Elixir is performed via pattern matching and head|tail
+  #  splitting between definition of a function's arity. This is, of course,
+  #  how someone immersed in the OOP-paradigm would describe a functional
+  #  language :). See the end of chapter 10 and beginning of chapter 11
+  #  for detailed info.
+  # Reduce a list of solar flare Maps to a single value, recursively, beginning
+  #  with this function signature.
+  def total_flare_power(list) do
+    # Call self, passing the initial value of the accumulator, as would be done
+    #  for a reduce method (in Ruby, JavaScript, etc).
+    total_flare_power list, 0
+  end
+  # Define the final statement in the recursive pattern next, as this is the exit
+  #  point of the function.
+  def total_flare_power([], total), do: total
+  # Next, define what happens when the List is non empty. Split the List into a
+  # `head` and `tail`, add the `head` to the accumulated value, and perform the
+  #  process again on the `tail`.
+  #
+  # def total_flare_power([head | tail], total) do
+  #   new_total = power(head) + total
+  #   total_flare_power(tail, new_total)
+  # end
+  #
+  # More calculations / adjustments are necessary, based on the "type" of flare
+  #  in the list. Instead of simply creating a `head`, pattern match against
+  #  the list item itself(Map, in this case), and perform each individual
+  #  adjustment.
+  # Create a pattern matching signature for the `M` class flare.
+  def total_flare_power([%{classification: :M, scale: s} | tail], total) do
+    new_total = s * 10 * 0.92 + total
+    total_flare_power(tail, new_total)
+  end
+  # ...repeat for `C` class flares...
+  def total_flare_power([%{classification: :C, scale: s} | tail], total) do
+    new_total = s * 0.78 + total
+    total_flare_power(tail, new_total)
+  end
+  # ...and repeat once more for `X` class flares.
+  def total_flare_power([%{classification: :X, scale: s} | tail], total) do
+    new_total = s * 1000 * 0.68 + total
+    total_flare_power(tail, new_total)
+  end
+
 end
