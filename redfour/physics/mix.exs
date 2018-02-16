65478835cf53b2733244d1d2dcc6f1a3fa350570
Todd Morningstar <techarchist@gmail.com>
2018-02-15 22:00:15 -0500

    Add notes for chapter 14 intro

--- redfour/physics/mix.exs
+++ redfour/physics/mix.exs
@@ -1,3 +1,19 @@
+# This `.exs` file is an Elixir Script, which is compiled at runtime.
+# Mix is the following:
+#  - a compiler
+#  - a test runner
+#  - a task runner
+#  - a generator
+# To contrast with IEX :
+#  - REPL
+#  - runs tasks interactively
+# This Mix file could be run by IEX as:
+#  - `iex -S mix`
+#  Then, when IEX is loaded, type:
+#  - `:observer.start`
+#  ... and behold the Erland process tree and all kinds of useful, visualized
+#  info about the application's runtime. All info is on the running processes
+#  running on the current Erland VM.
 defmodule Physics.Mixfile do
   use Mix.Project
 
@@ -22,7 +38,7 @@ defmodule Physics.Mixfile do
   defp deps do
     [
       {:timex, "~> 2.1.4"}, # This learning video was created ca. Elixir 1.2 (before time datatypes)
-      {:amnesia, "~> 0.2.7"} # Note that using the github repo (as in ch 13) is broken. DL from Hex. 
+      {:amnesia, "~> 0.2.7"} # Note that using the github repo (as in ch 13) is broken. DL from Hex.
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
