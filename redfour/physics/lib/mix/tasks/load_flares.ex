4ccfc73cc2a962ba5f1f7d495bb1877110637f2a
Todd Morningstar <techarchist@gmail.com>
2018-02-21 23:55:15 -0500

    Underscore args

--- redfour/physics/lib/mix/tasks/load_flares.ex
+++ redfour/physics/lib/mix/tasks/load_flares.ex
@@ -5,7 +5,10 @@ defmodule Mix.Tasks.Physics.LoadFlares do
 
   use Mix.Task
 
-  def run(args) do
+  # Prefix `args` with an underscore to allow the Elixir compiler to recognize
+  #  that we intend not to use it (and no need to emit a warning about an
+  #  unused var).
+  def run(_args) do
     IO.inspect "HI!"
   end
 end
