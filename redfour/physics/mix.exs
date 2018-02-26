9ddcf9c19e601ee6bd78c5081d880577fcf69785
Todd Morningstar <techarchist@gmail.com>
2018-02-25 21:13:11 -0500

    Bump app minor version

--- redfour/physics/mix.exs
+++ redfour/physics/mix.exs
@@ -20,7 +20,7 @@ defmodule Physics.Mixfile do
   def project do
     [
       app: :physics,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps()
